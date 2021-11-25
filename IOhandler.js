4c68ac924cb677279411589dc5ad5a8f6717ab71
chris <77702776+cchan45@users.noreply.github.com>
2021-11-24 22:57:38 -0800

    added grey filter

--- IOhandler.js
+++ IOhandler.js
@@ -69,14 +69,18 @@ const grayScale = (pathIn, pathOut, file) => {
           for (var y = 0; y < this.height; y++) {
             for (var x = 0; x < this.width; x++) {
               var idx = (this.width * y + x) << 2;
+              let grey = parseInt((this.data[idx] + this.data[idx+1] + this.data[idx +2]) /3)
 
               // invert color
-              this.data[idx] = 255 - this.data[idx]
-              this.data[idx + 1] = 255 - this.data[idx + 1]
-              this.data[idx + 2] = 255 - this.data[idx + 2]
+              this.data[idx] = grey
+              this.data[idx + 1] = grey
+              this.data[idx + 2] = grey
+              // this.data[idx] = 0 - this.data[idx] /3
+              // this.data[idx + 1] = 0 - this.data[idx + 1] /3
+              // this.data[idx + 2] = 0 - this.data[idx + 2] /3
 
               // and reduce opacity
-              this.data[idx + 3] = this.data[idx + 3] >> 1
+              // this.data[idx + 3] = this.data[idx + 3]
             }
           }
           this.pack().pipe(fs.createWriteStream(pathOut))
